125f27160bc000e40f0552835a1e4d0bb13e6821
pwindy <1211413788@qq.com>
2024-05-13 16:26:08 +0800

    commit screen

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+( function(){
+    var $sjglq = $('#sjglq');
+    var $ydltj = $('#ydltj');
+    var $ndltj = $('#ndltj');
+    var $yjydl = $('#yjydl');
+    $buyA.on( 'mouseenter mouseleave',function(){
+        $(this).toggleClass('hover');
+        $hide.stop().toggle();
+    } )
+}() );
+
+
